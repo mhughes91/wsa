382461cf30b30749a5870e5776cb357a630d87bb
MatttYews <mhughes91@hotmail.co.uk>
2019-10-12 14:27:48 +0100

    Added dependecies and package structure

---
+++ src/main/java/com/wsa/Main.java
@@ -0,0 +1,11 @@
+package com.wsa;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    
+    public static void main(String[] args) {
+
+    }
+}
